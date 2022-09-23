8c71e75271538147055cecb7f0467c84b440a7e7
milliorn <scottmilliorn@gmail.com>
2022-09-23 12:51:31 -0700

    TaskItem, styles, text

--- task-list/src/components/TaskItem.jsx
+++ task-list/src/components/TaskItem.jsx
@@ -1,30 +1,45 @@
 import { FaPencilAlt, FaTimes } from "react-icons/fa";
 
+const styles = {
+  divMain:
+    "flex justify-between max-w-full px-5 py-3 mx-auto my-2 overflow-scroll rounded-md cursor-pointer item bg-zinc-100",
+  divLeft: "text-zinc-900",
+  divParagraph: "sm:text-lg md:text-xl lg:text-2xl xl:text-3xl",
+  divSpan: "font-bold itemBold",
+  delete: "my-1.5 text-red-500 cursor-pointer delIcon",
+  edit: "my-1.5 text-blue-500 cursor-pointer editIcon",
+};
+
+const text = {
+  task: "Tasks: ",
+  time: "Time: ",
+};
+
 const TaskItem = ({ item, onDelete, onEdit }) => {
   return (
     <div>
-      <div className="flex justify-between max-w-full px-5 py-3 mx-auto my-2 overflow-scroll rounded-md cursor-pointer item bg-zinc-100">
-        <div className="text-zinc-900">
-          <p className="sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
-            <span className="font-bold itemBold">Item: </span>
+      <div className={styles.divMain}>
+        <div className={styles.divLeft}>
+          <p className={styles.divParagraph}>
+            <span className={styles.divSpan}>{text.task}</span>
             <span>{item.text}</span>
           </p>
-          <p className="sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
-            <span className="font-bold itemBold">Quantity: </span>
+          <p className={styles.divParagraph}>
+            <span className={styles.divSpan}>{text.time}</span>
             {item.quantity}
           </p>
         </div>
         <div>
-          <p className="sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
+          <p className={styles.divParagraph}>
             <FaTimes
               onClick={() => onDelete(item.id)}
-              className="my-1.5 text-red-500 cursor-pointer delIcon"
+              className={styles.delete}
             />
           </p>
-          <p className="sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
+          <p className={styles.divParagraph}>
             <FaPencilAlt
               onClick={() => onEdit(item.id)}
-              className="my-1.5 text-blue-500 cursor-pointer editIcon"
+              className={styles.edit}
             />
           </p>
         </div>
