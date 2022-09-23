5af5dde026dd41912f1bd13a039791f30effce0f
milliorn <scottmilliorn@gmail.com>
2022-09-23 12:25:15 -0700

    Button styles

--- task-list/src/components/Button.jsx
+++ task-list/src/components/Button.jsx
@@ -1,9 +1,14 @@
+const styles = {
+  button:
+    "inline-block px-5 py-3 m-1 text-sm no-underline border border-none rounded-md cursor-pointer sm:text-base btn bg-zinc-900 text-zinc-50 focus:text-zinc-50 focus:outline-none md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl",
+};
+
 const Button = ({ color, text, onClick }) => {
   return (
     <button
       onClick={onClick}
       style={{ backgroundColor: color }}
-      className="inline-block px-5 py-3 m-1 text-sm no-underline border border-none rounded-md cursor-pointer sm:text-base btn bg-zinc-900 text-zinc-50 focus:text-zinc-50 focus:outline-none md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl"
+      className={styles.button}
     >
       {text}
     </button>
