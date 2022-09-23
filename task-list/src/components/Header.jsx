50603c024e93dce4f8bececf3c0be2060e0f8d5a
milliorn <scottmilliorn@gmail.com>
2022-09-23 12:29:25 -0700

    Header, text, styles

--- task-list/src/components/Header.jsx
+++ task-list/src/components/Header.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import Button from "./Button";
 
+const styles = {
+  header: "flex items-center justify-between mb-4 header",
+  h2: "font-serif text-xl app-header sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl",
+};
+
+const text = {
+  h2: "Task List",
+};
+
 const Header = ({ showForm, changeTextAndColor }) => {
   return (
-    <header className="flex items-center justify-between mb-4 header">
-      <h2 className="font-serif text-xl app-header sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
-        Task List
-      </h2>
+    <header className={styles.header}>
+      <h2 className={styles.h2}>{text.h2}</h2>
       <Button
-        color={changeTextAndColor ? "red" : "green"}
+        color={changeTextAndColor ? "red" : "green"} // change color based on state
         onClick={showForm}
-        text={changeTextAndColor ? "Close" : "Add"}
+        text={changeTextAndColor ? "Close" : "Add"} // change color based on state
       />
     </header>
   );
